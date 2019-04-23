6d89f0e12bdcc714c14c57fb6c767149940a36de
billy-mcgrath <william.f.mcgrath.20@dartmouth.edu>
2019-04-23 15:11:46 -0400

    all the files for ws

---
+++ build/precache-manifest.b1513bb58427f275c5f9b2453a3130ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23d88c4b9cfc7feff21d1baea3c04261",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9fbafece22d7e9955d6",
+    "url": "/static/css/main.8cc4baf1.chunk.css"
+  },
+  {
+    "revision": "4270d9f223e41b8dbe93",
+    "url": "/static/js/2.91619cf3.chunk.js"
+  },
+  {
+    "revision": "c9fbafece22d7e9955d6",
+    "url": "/static/js/main.0825b668.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
